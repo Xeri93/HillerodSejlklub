b4cdf27d72321b9a3b304cee29057f4c571aa636
Xeri93 <mam015@edu.zealand.dk>
2024-12-06 13:01:33 +0100

    MaxTest

--- HillerodSejlklub/obj/Debug/net8.0/HillerodSejlklub.AssemblyInfo.cs
+++ HillerodSejlklub/obj/Debug/net8.0/HillerodSejlklub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fc97eea3e4e79d355b858058476ee5bbfd0ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022e96c4d4c5b0d5c7e914250bb97d2f5aa58700")]
 [assembly: System.Reflection.AssemblyProductAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
