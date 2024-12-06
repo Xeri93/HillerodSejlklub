77fc97eea3e4e79d355b858058476ee5bbfd0ad1
Tobias Daniel Bertelsen <tob004@edu.zealand.dk>
2024-12-06 12:18:56 +0100

    test

--- HillerodSejlklub/obj/Debug/net8.0/HillerodSejlklub.AssemblyInfo.cs
+++ HillerodSejlklub/obj/Debug/net8.0/HillerodSejlklub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a268a8b0f1a4b02051233f156a59f7f9391294")]
 [assembly: System.Reflection.AssemblyProductAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HillerodSejlklub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
